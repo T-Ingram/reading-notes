c864da2d25fab6eb34e5e5e88d9f9fd0239690a1
Trevor Ingram <ingramtrevor10@gmail.com>
2023-08-22 18:46:09 -0700

    started over again with js file

--- 102/index.js
+++ 102/index.js
@@ -1,26 +1,10 @@
-// script.js
+// index.js
 
-function displayGreeting() {
-    const userName = document.getElementById("userName").value;
-    const greetingMessage = document.getElementById("greetingMessage");
-    greetingMessage.textContent = `Hello, ${userName}! Welcome to our oil change guide.`;
-}
-
-function displayInstructions() {
-    const transmissionType = document.getElementById("transmission").value;
-    const instructionMessage = document.getElementById("instructionMessage");
-
-    if (transmissionType === "automatic") {
-        instructionMessage.textContent = "For automatic transmission vehicles, follow these instructions...";
-    } else if (transmissionType === "manual") {
-        instructionMessage.textContent = "For manual transmission vehicles, follow these instructions...";
-    } else {
-        instructionMessage.textContent = "";
-    }
-}
-
-function updateDate() {
+function displayCurrentDate() {
     const currentDateElement = document.getElementById("currentDate");
     const currentDate = new Date();
     currentDateElement.textContent = `Today's Date: ${currentDate.toDateString()}`;
 }
+
+// Call the function to display the date when the page loads
+displayCurrentDate();
