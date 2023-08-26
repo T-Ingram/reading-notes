cf3718996b02ddbd45b1fa20751cc6748cd9c053
Trevor Ingram <ingramtrevor10@gmail.com>
2023-08-26 10:12:21 -0700

    deleted image prompt

--- 102/index.js
+++ 102/index.js
@@ -34,16 +34,8 @@ function displayInstructions() {
         }
     } else if (selectedTransmission === "manual") {
         instructionMessage.textContent = "For manual transmission vehicles, follow these instructions...";
-                // Ask the user for the number of times to display the image
-                const numImages = parseInt(prompt("How many times would you like to see the image?"));
-
-        if (!isNaN(numImages) && numImages > 0) {
-                    // Display the image 'numImages' times
-            displayImages(numImages);
-            instructionMessage.textContent = "";
+       // Valid transmission type selected
         } else {
-            instructionMessage.textContent = "Please enter a valid number greater than 0.";
-    } else {
         // Invalid selection, show an error message
         instructionMessage.textContent = "Please select a valid transmission type.";
         transmissionSelect.value = ""; // Clear the selection
