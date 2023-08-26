a68cbd88942944af31f0f4d4864a670a983032f4
Trevor Ingram <ingramtrevor10@gmail.com>
2023-08-26 10:36:48 -0700

    added new transmission selection type function to manual

--- 102/index.js
+++ 102/index.js
@@ -23,11 +23,19 @@ function displayInstructions() {
         instructionMessage.textContent = "For automatic transmission vehicles, follow these instructions...";
         // Valid transmission type selected
         // Ask the user for the number of times to display the image
-        const numImages = parseInt(prompt("How many times would you like to see the image?"));
+      
     } else if (selectedTransmission === "manual") {
         instructionMessage.textContent = "For manual transmission vehicles, follow these instructions...";
         // Valid transmission type selected
+            // Ask the user for the number of times to display the image
         const numImages = parseInt(prompt("How many times would you like to see the image?"));
+        if (!isNaN(numImages) && numImages > 0) {
+            // Display the image 'numImages' times
+            displayImages(numImages);
+            instructionMessage.textContent = "";
+        } else {
+            instructionMessage.textContent = "Please enter a valid number greater than 0.";
+        }
         } else {
         // Invalid selection, show an error message
         instructionMessage.textContent = "Please select a valid transmission type.";
