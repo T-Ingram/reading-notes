777006e0f2d19eeb869286b17ac4dbed76715d1d
Trevor Ingram <ingramtrevor10@gmail.com>
2023-08-26 10:56:43 -0700

    added div image-section

--- 102/index.js
+++ 102/index.js
@@ -11,6 +11,19 @@ function displayGreeting() {
     greetingMessage.textContent = `Hello, ${userName}! Welcome to your oil change guide!`;
 }
 
+function displayCurrentDate() {
+    const currentDateElement = document.getElementById("currentDate");
+    const currentDate = new Date();
+    currentDateElement.textContent = `Today's Date: ${currentDate.toDateString()}`;
+}
+displayCurrentDate();
+
+function displayGreeting() {
+    const userName = document.getElementById("userName").value;
+    const greetingMessage = document.getElementById("greetingMessage");
+    greetingMessage.textContent = `Hello, ${userName}! Welcome to your oil change guide!`;
+}
+
 function displayInstructions() {
     const transmissionSelect = document.getElementById("transmission");
     const selectedTransmission = transmissionSelect.value;
@@ -24,7 +37,7 @@ function displayInstructions() {
         // Valid transmission type selected
         // Ask the user for the number of times to display the image
         const numImages = parseInt(prompt("How many times would you like to see the image?"));
-        
+
         if (!isNaN(numImages) && numImages > 0) {
             // Display the image 'numImages' times
             displayImages(numImages);
@@ -35,12 +48,23 @@ function displayInstructions() {
     } else if (selectedTransmission === "manual") {
         instructionMessage.textContent = "For manual transmission vehicles, follow these instructions...";
         // Valid transmission type selected
-
-
-        } else {
+    } else {
         // Invalid selection, show an error message
         instructionMessage.textContent = "Please select a valid transmission type.";
         transmissionSelect.value = ""; // Clear the selection
+    }
+}
+
+function displayImages(numImages) {
+    const imageSection = document.querySelector(".image-section");
+    imageSection.innerHTML = ""; // Clear any previous images
+
+    for (let i = 0; i < numImages; i++) {
+        const img = document.createElement("img");
+        img.src = "https://images.unsplash.com/photo-1603598154505-0192e5365a35?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1173&q=80";
+        img.alt = "Toyota Tacoma";
+        img.width = 600;
+        imageSection.appendChild(img);
     }
 }
 
